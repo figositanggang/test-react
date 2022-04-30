9d3f58d339a067a01425794f0f1771b135ac5964
unLinked#666 <hamonangansitanggang29@gmail.com>
2022-04-30 19:18:12 +0700

    aww

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import './App.css';
+import "./App.css";
+import React, { Component } from "react";
+import axios from "axios";
 
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-function App() {
-  return (
-    <div className="App">
-      Hello HTML, I'm Backs
-    </div>
-  );
+    //
+    this.state = {
+      users: [],
+    };
+  }
+
+  componentDidMount() {
+    axios("https://api.randomuser.me/?nat=US&results=10").then((response) =>
+    this.setState({users: response.data.results})
+    );
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.users.map((user) => (
+          <p>{user.email}</p>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
